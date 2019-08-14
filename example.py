b2cf060b07b5e5eb23e8fb39e8495e026351ab28
JCSC97 <jcesar_csc@hotmail.com>
2019-08-14 18:45:36 -0500

    Minor

--- example.py
+++ example.py
@@ -4,4 +4,4 @@ import time
 hrs = time.strftime("%H:%M:%S")
 date = time.strftime("%d-%m-%y")
 print("El script fue ejecutado a las : "+hrs)
-print("en el día : "+date)
+print("en la fecha : "+date+", del servidor")
