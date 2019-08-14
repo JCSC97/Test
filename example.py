8a1eccfc6defd759d3d1d377aa75ee7c6d5ba202
JCSC97 <52362772+JCSC97@users.noreply.github.com>
2019-08-14 18:28:57 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,5 @@
 import time
-hrs=time.strftime("%H:%M:%S")
-print(hrs)
+hrs = time.strftime("%H:%M:%S")
+date = time.strftime("%d/%m/%y")
+print("El script fue ejecutado a las : "hrs)
+print("en el día : "hrs)
