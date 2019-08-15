28ef9c0b4a274241fcb17f60ce749a65c7bc1ff7
JCSC97 <jcesar_csc@hotmail.com>
2019-08-15 16:12:30 -0500

    Minor

--- example.py
+++ example.py
@@ -4,4 +4,4 @@ import time
 hrs = time.strftime("%H:%M:%S")
 date = time.strftime("%d-%m-%y")
 print("El script fue ejecutado a las : "+hrs)
-print("en la fecha : "+date+", del servidor de jenkins")
+print("en la fecha : "+date+", del servidor de jenkins de ohio")
